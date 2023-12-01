4bdc055ac0b9f386c8278ccb80731c5ad00457d8
champslays <championosai@gmail.com>
2023-12-01 13:57:21 -0500

    added shiesty

--- javascript.js
+++ javascript.js
@@ -1,20 +1,12 @@
-
 function onClick(inputValue) {
-  document.getElementById("displayedText").innerHTML = "JUMP SCARE!!!!!!!!!!!";
-  const user = {name: 'Bob'};
-  console.log('Hello ' + user.name);
 
-  const names = ['Sally', 'Jerome']
-  const users = [{name: 'Sally'}, {name: 'Jerome'}]
-  console.log ('First name in list -----',names[0]);
-
-  console.log ('Second name in list of objects -----',users[1].name);
+  
   const url = 'https://pokeapi.co/api/v2/pokemon';
   $.get(url,function(data, status){
       if (status === 'success') {
           const pokemonNames = data.results.map(showNames)
           console.log(pokemonNames);
-          document.getElementById("displayedText").innerHTML = pokemonNames.join('      ');
+          document.getElementById("displayedText").innerHTML = pokemonNames.join('   ');
       }
   });
   alert('Hello, ' + inputValue + '!');
